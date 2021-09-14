9281b849b315ac93254d308237fdf50ef4013972
Moses Berra <moses.r.berra@gmail.com>
2021-09-14 15:39:16 -0500

    add searchDatabases and getDatabase to notion client wrapper

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,8 +1,36 @@
 import { Client } from '@notionhq/client';
 import { NotionPage } from '../models/notion-page';
-import { deserializePage } from './deserializers';
+import { NotionDatabase } from '../models/notion-database';
+import { deserializeDatabase, deserializePage } from './deserializers';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+export async function searchDatabases(query: string):Promise<NotionDatabase[]> {
+  try {
+    const response =  await notion.search({
+      query,
+      sort: {
+        direction: 'ascending',
+        timestamp: 'last_edited_time',
+      },
+    });
+    const serializedDatabases = response.results.filter(result => result["object"] === "database");
+    return serializedDatabases.map(db => deserializeDatabase(db));
+  } catch (error) {
+    console.error(`Error calling Notion API to search for: "${query}"`, error);
+    throw(error);
+  }
+};
+
+export async function getDatabase(databaseId: string):Promise<NotionDatabase> {
+  try {
+    const response = await notion.databases.retrieve({ database_id: databaseId });
+    return deserializeDatabase(response);
+  } catch (error) {
+    console.error(`Error calling Notion API to get database "${databaseId}"`, error);
+    throw(error);
+  }
+};
 
 export async function createPage(pageToCreate: NotionPage):Promise<NotionPage> {
   try {
--- tests/lib/notion.test.ts
+++ tests/lib/notion.test.ts
@@ -3,7 +3,7 @@ import { PropertyType as _PropertyType } from '../../models/property-type';
 const PropertyType = { ..._PropertyType };
 import { TitleProperty, RichTextProperty } from "../../models/page-property";
 import { NotionPage } from '../../models/notion-page'
-import { createPage } from "../../lib/notion";
+import { createPage, getDatabase, searchDatabases } from "../../lib/notion";
 
 const mockCreatedPage = {
   "object": "page",
@@ -58,20 +58,120 @@ const mockCreatedPage = {
     }
   }
 }
+const mockRetrievedDatabaseOne = {
+  "id": "dddddddd-dddd-dddd-dddd-111111111111",
+  "title": [
+    {
+      "type": "text",
+      "text": {
+        "content": "Grocery List",
+        "link": null
+      },
+      "plain_text": "Grocery List",
+      "href": null
+    }
+  ],
+  "properties": {}
+}
+const mockRetrievedDatabaseTwo = {
+  "id": "dddddddd-dddd-dddd-dddd-222222222222",
+  "title": [
+    {
+      "type": "text",
+      "text": {
+        "content": "Packing List",
+        "link": null
+      },
+    }
+  ],
+  "properties": {}
+}
+const mockSearchedDatabases = [
+  Object.assign({ "object": "database" }, mockRetrievedDatabaseOne),
+  Object.assign({ "object": "database" }, mockRetrievedDatabaseTwo)
+]
 const mockPageCreate = jest.fn().mockResolvedValue(mockCreatedPage);
-
+const mockDatabaseRetrieve = jest.fn().mockResolvedValue(mockRetrievedDatabaseOne)
+const mockSearch = jest.fn().mockResolvedValue({ "results": mockSearchedDatabases });
 jest.mock("@notionhq/client", () => {
   return {
     Client: jest.fn().mockImplementation(() => {
       return {
         // TODO: This is not the most *correct* way, but it works. Probably worth revisiting.
         pages: { create:  (...args: Parameters<typeof mockPageCreate>) => mockPageCreate(...args) },
+        databases: { retrieve:  (...args: Parameters<typeof mockDatabaseRetrieve>) => mockDatabaseRetrieve(...args) },
+        search: (...args: Parameters<typeof mockSearch>) => mockSearch(...args)
       }
     })
   }
 })
 const mockedClient = Client as unknown as jest.Mock<Client>;
 let sharedVars = new Object();
+
+describe('searchDatabases', () => {
+  beforeEach(() => {
+    mockedClient.mockClear();
+    mockSearch.mockClear();
+  });
+
+  describe('calls the notion client', () => {
+    test('with the provided search term', async () => {
+      await searchDatabases("something");
+      expect(mockSearch).toHaveBeenCalledTimes(1);
+      expect(mockSearch).toHaveBeenCalledWith({
+        query: "something",
+        sort: {
+          direction: 'ascending',
+          timestamp: 'last_edited_time',
+        }
+      });
+    });
+
+    test('creates new NotionDatabase objects from the returned JSON', async () => {
+      const returnedNotionDbs = await searchDatabases('list');
+      expect(returnedNotionDbs.map(db => db.id)).toHaveLength(2);
+      expect(returnedNotionDbs.map(db => db.id)).toEqual(expect.arrayContaining([
+        "dddddddd-dddd-dddd-dddd-111111111111",
+        "dddddddd-dddd-dddd-dddd-222222222222"
+      ]));
+    });
+
+    test('and throws if it receives an error', async () => {
+      expect.assertions(1);
+      jest.spyOn(console, 'error').mockImplementation(() => {});
+      mockSearch.mockRejectedValueOnce(new Error("error"));
+      await expect(searchDatabases('something')).rejects.toThrow();
+    });
+  });
+});
+
+describe('getDatabase', () => {
+  beforeEach(() => {
+    mockedClient.mockClear();
+    mockDatabaseRetrieve.mockClear();
+  });
+
+  describe('calls the notion client', () => {
+    test('with the provided database ID', async () => {
+      await getDatabase("dddddddd-dddd-dddd-dddd-111111111111");
+      expect(mockDatabaseRetrieve).toHaveBeenCalledTimes(1);
+      expect(mockDatabaseRetrieve).toHaveBeenCalledWith({ database_id: "dddddddd-dddd-dddd-dddd-111111111111" });
+    });
+
+    test('creates a new NotionDatabase object from the returned JSON', async () => {
+      const returnedNotionDb = await getDatabase("dddddddd-dddd-dddd-dddd-111111111111");
+      expect(returnedNotionDb.id).toEqual("dddddddd-dddd-dddd-dddd-111111111111");
+      expect(returnedNotionDb.title).toEqual("Grocery List");
+    });
+
+    test('and throws if it receives an error', async () => {
+      expect.assertions(1);
+      jest.spyOn(console, 'error').mockImplementation(() => {});
+      mockSearch.mockRejectedValueOnce(new Error("error"));
+      await expect(searchDatabases('something')).rejects.toThrow();
+    });
+  });
+});
 
 describe('createPage', () => {
   beforeEach(() => {
@@ -118,7 +218,7 @@ describe('createPage', () => {
         .toEqual(expect.arrayContaining([PropertyType.Title, PropertyType.RichText, PropertyType.RichText]));
       expect(returnedNotionPage.pageProperties.map(p => p.simplifiedValue))
         .toEqual(expect.arrayContaining(["Home", "123", "Sesame St"]));
-    })
+    });
 
     test('and throws if it receives an error', async () => {
       expect.assertions(1);
